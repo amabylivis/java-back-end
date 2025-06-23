d6878fb33ccaa01c616e1d5a474551f219de161e
Amabyli Visnheski <158176703+amabylivis@users.noreply.github.com>
2025-06-23 10:09:40 -0300

    commit inicial

---
+++ src/Aula01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Aula01 {
+    //constantes, deve ser em maiusculo e antes do main
+    private final static String WELCOME_MESSAGE = "Olá! Informe o seu nome: ";
+
+    public static void main(String[] args) {
+
+        //declarando variáveis
+        Scanner scanner = new Scanner(System.in);
+
+        //tela e comando para receber valores
+
+        System.out.println(WELCOME_MESSAGE);
+        String name = scanner.next();
+        System.out.println("Informe sua idade: ");
+        int age = scanner.nextInt();
+        System.out.printf("Olá %s sua idade é %s \n", name, age);
+
+        //tipagem estática, declarou uma variável não pode mudar
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        String msg = "Programando em: ";
+        System.out.println("Hello World.");
+        System.out.println(msg + args[0]);
+        System.out.println("Precisamos ter uma " + args[1]);
+    }
+
+}
